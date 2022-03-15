d943bed4fe0fe56ed2503f4b91fbf337c612a001
rainasong <rainasong0509@gmail.com>
2022-03-15 22:51:18 +1300

    Add basic function structures

---
+++ forgettable-frontend/src/services/services.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import axios from 'axios';
+
+export const getPerson = (personId) => {
+
+};
+
+export const getAllPersons = () => {
+
+};
+
+export const createPerson = (person) => {
+  // {
+  //     full_name: string,
+  //     birthday?: Date,
+  //     gender?: string,
+  //     location?: string,
+  //     first_met?: Date,
+  //     how_we_met?: string,
+  //     interests?: string[],
+  //     organisation?: string,
+  //     social_media?: Map<string>,
+  //     image?: base64-image,
+  //     encounters?: ObjectId[],
+  //     time_updated: Date
+  //   }
+
+};
+
+export const updatePerson = (person) => {
+
+};
+
+export const deletePerson = (personId) => {
+
+};
+
+export const getEncounter = (encounterId) => {
+
+};
+
+export const getAllEncounters = () => {
+
+};
+
+export const createEncounter = (encounter) => {
+  // {
+  //     date:? Date,
+  //     location?: string,
+  //     title: string,
+  //     description?: string,
+  //     persons?: [ObjectId]
+  //   }
+};
+
+// export const updateEncounter = () => {
+// do we want this?
+// }
+
+export const deleteEncounter = (encounterId) => {
+
+};
